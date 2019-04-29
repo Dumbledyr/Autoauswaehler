c736c86116e6a4ad23bcbe6bf45173eb754c9c61
Dumbledyr <49987222+Dumbledyr@users.noreply.github.com>
2019-04-29 17:00:11 +0200

    Zufällige Ausgabe eines Fahrzeugs (Test)

--- content/Ergebnis.php
+++ content/Ergebnis.php
@@ -6,21 +6,21 @@
 		<title>Auto-Finder</title>
 		<h1 align="center" >Auto-Finder</h1>
 </head>
-<body bgcolor="#green">
+<body bgcolor="#afafaf">
 	<p align="center"><Font Size="10">Dieses Auto passt am besten zu ihnen:</Font></p>
 	
 	<?php include("../register/connect.php"); 
 
-	$sql = "SELECT * FROM auto ORDER BY RAND() LIMIT 1";
+	$sql = "SELECT marke,modell FROM auto ORDER BY RAND() LIMIT 1";
 	
 	$tst = mysqli_query ($con, $sql);
 	
-
-	while($row = mysqli_fetch_array($tst))
+	while($row = mysqli_fetch_all($tst))
      {
-        print_r($row);
-     } 
 
+        print_r($row);
+     
+     }
 	?>
 	
 
